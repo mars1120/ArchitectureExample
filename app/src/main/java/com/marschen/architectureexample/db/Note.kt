98db972e0d61ee843d3a59fa02a7281eaf118035
marschen <peach414@gmail.com>
2020-02-21 16:45:25 +0800

    dependency Room and Viewmodel

---
+++ app/src/main/java/com/marschen/architectureexample/db/Note.kt
@@ -0,0 +1,11 @@
+package com.marschen.architectureexample.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "note_table")
+data class Note(val title: String, val description: String, val priority: Int) {
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
+}
